443fd27cc33ebcb80d997b427ee2729ffbc07cfe
edgeremmanuel <edgeremmanuel0866@gmail.com>
2025-03-07 23:16:56 +0300

    changes

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
-import {initializedApp} from "https://playground-a7af0-default-rtdb.europe-west1.firebase-app.js"
-import {getDatabase, ref, push } from "https://playground-a7af0-default-rtdb.europe-west1.firebase-database.app/"
+import {initializedApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import {getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 
 const appSettings ={
-  databaseURL: "https://playground-a7af0-default-rtdb.europe-west1.firebasedatabase.app/"
+  databaseURL: "https://realtime-database-bb8d3-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 const app = initializedApp(appSettings)
 const database = getDatabase(app) 
-const moviesInDB = ref(database, movies)
+const shoppinglistInDB = ref(database, "shoppinglist")
 
 console.log(app)
 
 const inputFieldEl = document.getElementById("input-field")
  const addButtonEl = document.getElementById("add-button")
+ const shoppinglistEl = document.getElementById("shopping-list")
 
- addButtonEl.addEventListener("click", function(){
+ addButtonEl.addEventListener("click", function() {
    let inputValue = inputFieldEl.value
     console.log(inputFlieldEl.value)
 
-    push(moviesInDB, inputValue)   
-    console.log('${inputValue} added to database')
+     push(shoppinglistInDB, inputValue)
+
+     inputFieldEl.value = ""
+    shoppinglistEl.innerHTML += '<li>${inputValue}</li>'
  })
  
