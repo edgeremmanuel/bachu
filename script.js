b49593c2dea437a95a75c3039e5f28a0a6862624
edgeremmanuel <edgeremmanuel0866@gmail.com>
2025-03-07 14:02:33 +0300

    changes

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
- const inputFieldEl = document.getElementById("input-field")
+import {initializedApp} from "https://playground-a7af0-default-rtdb.europe-west1.firebase-app.js"
+import {getDatabase, ref, push } from "https://playground-a7af0-default-rtdb.europe-west1.firebase-database.app/"
+
+
+const appSettings ={
+  databaseURL: "https://playground-a7af0-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+const app = initializedApp(appSettings)
+const database = getDatabase(app) 
+const moviesInDB = ref(database, movies)
+
+console.log(app)
+
+const inputFieldEl = document.getElementById("input-field")
  const addButtonEl = document.getElementById("add-button")
 
  addButtonEl.addEventListener("click", function(){
    let inputValue = inputFieldEl.value
     console.log(inputFlieldEl.value)
+
+    push(moviesInDB, inputValue)   
+    console.log('${inputValue} added to database')
  })
  
